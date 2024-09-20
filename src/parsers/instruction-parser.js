4b84a70d4d925801d9c4f0029d4a0f82e1a2f1e0
Mathias Capdet <mathias.capdet@tescan.com>
2024-09-20 12:10:39 +0200

    Update instruction data object

--- src/parsers/instruction-parser.js
+++ src/parsers/instruction-parser.js
@@ -1,12 +1,16 @@
 import { Instruction } from '../models/instruction.js';
 
-export function parseInstruction(data) {
-    if (!data || !data.instruction || !data.type) {
-        throw new Error('Invalid instruction');
-    }
+export function InstructionParser() {}
+Object.assign(InstructionParser.prototype, {
+    parse: function(data) {
+        if (!data || !data.instruction || !data.type) {
+            throw new Error('Invalid instruction');
+        }
 
-    if (data.type !== 'instruction') {
-        throw new Error(`Invalid instruction type ${data.type}`);
+        if (data.type !== 'instruction') {
+            throw new Error(`Invalid instruction type ${data.type}`);
+        }
+
+        return new Instruction(data.instruction);
     }
-    return new Instruction(data.instruction);
-}
+});
